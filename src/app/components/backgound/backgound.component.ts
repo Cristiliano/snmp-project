0e148bad09f079f02fa4c7f5748f2f731647c165
Cristiliano <cristiliano.impulso@parceiro.elevaeducacao.com.br>
2022-04-27 02:56:43 -0300

    change speed video play

--- src/app/components/backgound/backgound.component.ts
+++ src/app/components/backgound/backgound.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-backgound',
   templateUrl: './backgound.component.html',
-  styleUrls: ['./backgound.component.scss']
+  styleUrls: ['./backgound.component.scss'],
 })
 export class BackgoundComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let video = document.getElementById('bgvid') as HTMLVideoElement;
+    video.defaultPlaybackRate = 0.1;
   }
-
 }
